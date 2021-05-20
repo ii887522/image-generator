2317b11b168f5bc12138c8976323bb8efb3cec2e
Chi Min Yong <chimin.yong@g2g.com>
2021-05-20 11:09:36 +0800

    Support generating transparent images

---
+++ image-generator/src/main/Any/constants.h
@@ -0,0 +1,13 @@
+// Copyright ii887522
+
+#ifndef IMAGE_GENERATOR_SRC_MAIN_ANY_CONSTANTS_H_
+#define IMAGE_GENERATOR_SRC_MAIN_ANY_CONSTANTS_H_
+
+namespace ii887522::imageGenerator {
+
+constexpr auto PROGRAM_NAME_INDEX{ 0u };
+constexpr auto OUTPUT_DIRECTORY_PATH_INDEX{ 1u };
+
+}  // namespace ii887522::imageGenerator
+
+#endif
